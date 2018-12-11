28871eb651f1dc82201ed90f26e7b01be24e16f9
Martin Borbola <borbolam14@gmail.com>
2018-12-12 00:25:13 +0100

    day 6 finally completed

---
+++ 2018/06/2.py
@@ -0,0 +1,51 @@
+import re
+import string
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = int(x)
+        self.y = int(y)
+
+def manhattan_distance(point1: Point, point2: Point) -> int:
+    return abs(point1.x - point2.x) + abs(point1.y - point2.y)
+
+def write_2dlist_to_file(list2d):
+    with open(r'map2', 'w') as f:
+        for outer_list in list2d:
+            for item in outer_list:
+                f.write(f'|{item}')
+            f.write('|\r')
+
+coordinatem = re.compile(r'(\d+), (\d+)')
+
+def main():
+    abc = string.ascii_letters
+    with open(r'input', 'r') as f:
+        coordinates = f.readlines()
+    referece_map = [['x' for j in range(360)] for i in range(360)]
+    coordinate_letter_map = {}
+    for idx, line in enumerate(coordinates):
+        x, y = coordinatem.match(line).group(1,2)
+        point = Point(x,y)
+        letter = abc[idx]
+        coordinate_letter_map[letter] = point
+        referece_map[point.x][point.y] = letter
+    map = [['x' for j in range(360)] for i in range(360)]
+    for x in range(360):
+        for y in range(360):
+            sum_distance = 0
+            for letter, point in coordinate_letter_map.items():
+                sum_distance += manhattan_distance(Point(x,y), point)
+            map[x][y] = '#' if sum_distance < 10000 else '.'
+                
+            
+    write_2dlist_to_file(map)
+    rows=''
+    for row in map:
+        rows = rows + ''.join(row)
+    abc = abc + '#'
+    for char in abc:
+        print(f'{char}: {rows.count(char)}')
+
+main()
+
