edfaba8970d5e7ccac842e44ffa17e93592a2584
Martin Borbola <borbolam14@gmail.com>
2018-12-17 00:38:02 +0100

    Finished 10th day. No need for 2.py

--- 2018/10/1.py
+++ 2018/10/1.py
@@ -2,6 +2,12 @@ import re
 import numpy as np
 from PIL import Image
 
+# First I searched for the tightest matrix after I move the points. This state happens after 10375 seconds
+# Then I wrote the matrix-ifier after that the print matrix
+# This way I was able to look at the matrix after the discovered seconds passed
+# And the puzzle is solved
+# This is a little bit backward thinking but it works so I don't have to rewrite this
+
 def generate_img(two_dimensional_list, second):
     for i in range(len(two_dimensional_list)):
         for j in range(len(two_dimensional_list[i])):
@@ -34,16 +40,31 @@ def make_matrix(points):
     maxx = max(xs)
     miny = min(ys)
     maxy = max(ys)
-    # print(minx, maxx, miny, maxy)
-    size = (maxx-minx) * (maxy-miny)
-    return size
+    print(minx,maxx, miny, maxy)
+    matrix = [['.' for _ in range(maxx-minx + 1)] for _ in range(maxy-miny + 1)]
+    print(len(matrix), len(matrix[0]))
+    for p in points:
+        print(f'X: {p["x"]}, Y: {p["y"]}')
+        matrix[p['y']-miny][p['x']-minx] = '#'
+    return matrix
 
 
 def print_points(points):
     pass
 
+
+def print_matrix(matrix):
+    for i in range(len(matrix)):
+        line = ''
+        for j in range(len(matrix[i])):
+            line += (matrix[i][j])
+        print(line)
+
 def make_seconds_pass(points, seconds):
-    pass
+    for _ in range(seconds):
+        for p in points:
+            p['x'] += p['vx']
+            p['y'] += p['vy']
 
 def next_second(points):
     for p in points:
@@ -52,12 +73,8 @@ def next_second(points):
 
 def main():
     points = read_input()
-    for s in range(11000):
-        if s > 10000:
-            print(f'{s}')
-            size = make_matrix(points)
-            if size < 10000:
-                print(f'size: {size}')
-        next_second(points)
+    make_seconds_pass(points, 10375)
+    matrix = make_matrix(points)
+    print_matrix(matrix)
 
 main()
